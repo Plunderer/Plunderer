abe4b96e16c11be96d53d0f16802ed1611dc5e70
Plunderer <plunderertech.c@gmail.com>
2016-09-07 13:17:31 +0900

    Update MoveToTarget.cs

--- Script/MoveToTarget.cs
+++ Script/MoveToTarget.cs
@@ -4,8 +4,6 @@ using System.Collections;
 /// 壁を回避して追ってくるスクリプトです。
 /// targetに追う対象を登録して使うです。
 /// NavMeshAgentと床の焼き付け的なのを登録しないと意味がないです。
-/// ｋｗｓｋは↓のURLにて
-/// 参考URL　http://qiita.com/AuraOtsuka/items/f6738963644faae25919
 /// </summary>
 public class MoveToTarget : MonoBehaviour {
     public Transform target;
