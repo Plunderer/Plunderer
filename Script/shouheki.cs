016df9b4e332ad6d74730e6647935bdcf2733a4a
Plunderer <plunderertech.c@gmail.com>
2016-09-07 17:06:16 +0900

    Update shouheki.cs

--- Script/shouheki.cs
+++ Script/shouheki.cs
@@ -1,42 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class shouheki : MonoBehaviour {
-//プレイヤーを進ませすぎないための障害となる障壁システム
-//一部の対応した敵を倒すか直接攻撃することで破壊できる
+public class shouheki : MonoBehaviour 
+{
+	//プレイヤーを進ませすぎないための障害となる障壁システム
+	//一部の対応した敵を倒すか直接攻撃することで破壊できる
 	public GameObject explosion;    //爆発エフェクト
 	public GameObject item;
 	private bool isQuitting = false;
-	public float life = 100; //敵の体力
+	public float life = 100;	//敵の体力
 	float bruntime, brundamage2, time;
-    // 固定ダメージ
-    //public int n = 15;
-    atari_Enemy atari;
-
-    GameObject refObj;
+	
+    	GameObject refObj;
     
-    public int hit = 0;
-	
-	//float Drop = 1;
-	
-	
-	//Dmage関数
-	
+    	public int hit = 0;
+    	
 	void start()
-    {
-        //refObj = GameObject.Find("atari_enemy");
+    	{
 	}
 	
 	void Update () 
-    {
+    	{
 		time += Time.deltaTime;
 		if (time >= 1) 
-        {
+        	{
 			if (bruntime > 0) 
-            {
+            		{
 				life -= brundamage2;
 				if(life <= 0)
-                {
+                		{
 					//体力が0以下になった時
 					Dead(); //死亡処理
 				}
@@ -44,28 +36,22 @@ public class shouheki : MonoBehaviour {
 			}
 			time = 0;
 		}
-        /*
-        if(hit == 10)
-        {
-            life -= n;
-        }*/
 	}
 	
 	public void Damage ( float damage ) 
-    {
+    	{
 		life -= damage; //体力から差し引く
 		if(life <= 0)
-        {
+        	{
 			//体力が0以下になった時
 			Dead(); //死亡処理
 		}
 	}
 	public void stunDamage ( float stundamage ) 
-    {
-
+    	{
 	}
 	public void brundamage ( float brundamage ) 
-    {
+    	{
 		bruntime = 5;
 		brundamage2 = brundamage;
 	}
@@ -73,31 +59,20 @@ public class shouheki : MonoBehaviour {
 	
 	//死亡処理
 	public void Dead () 
-    {
+    	{
 		if (item != null)
-        {
+        	{
 			Instantiate (item,transform.position,transform.rotation);
 		}
 		GameObject Player = GameObject.Find("PlayerMove");
 		GameObject.Instantiate(explosion, transform.position, Quaternion.identity); //爆発パーティクルを生成
 		Destroy(this.gameObject);   //自身を削除
 	}
-    /*
-    //連動処理
-    public void rendou()
-    {
-        life -= 15;
-    }
-
-	void OnApplicationQuit () 
-    {
-		isQuitting = true;
-	}*/
 	
 	void OnDestroy () 
-    {
+    	{
 		if(!isQuitting)
-        {
+        	{
 			GameObject.Instantiate(explosion, transform.position, Quaternion.identity);
 		}
 	}
