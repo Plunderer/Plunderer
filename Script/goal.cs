d86939a82e5ec0f4ab77027da1a7cb0a29849a30
yujinwatabe <15c1020043wy@edu.tech.ac.jp>
2016-09-08 17:09:10 +0900

    クリア時の演出
    ボスを撃破したタイミングで呼び出す

---
+++ Script/goal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class goal : MonoBehaviour {
+    AudioSource sound01;
+    // Use this for initialization
+    void Start () {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        sound01 = audioSources[0];
+        sound01.Play();
+        StartCoroutine("ending");
+    }
+
+    // Update is called once per frame
+    IEnumerator ending()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("title");
+    }
+}
